746a66fc5c415f2e6b69be8f258ad0a326f9b660
tonyatpeking <tonyatpeking@gmail.com>
2022-05-11 23:24:16 -0500

    eval works for all metrics

--- Final Project/re_cycle_gt-master/new_eval.py
+++ Final Project/re_cycle_gt-master/new_eval.py
@@ -39,14 +39,20 @@ def camelCaseSplit(identifier):
 
 
 
-def predict_samples(model_path, test_data_path, num_samples=10):
+def predict_samples(model_path, test_data_path, num_samples=1000, mode='g2t'):
     test_data = []
     with open(test_data_path, 'r', encoding='utf-8') as f:
         test_data = json.load(f)
-    test_df = new_preprocessing.json_to_df(test_data,
-                                           task_prompt='g2t: ',
-                                           source_name='graph',
-                                           target_name='text')
+    if mode == 'g2t':
+        test_df = new_preprocessing.json_to_df(test_data,
+                                            task_prompt='g2t: ',
+                                            source_name='graph',
+                                            target_name='text')
+    else:
+        test_df = new_preprocessing.json_to_df(test_data,
+                                               task_prompt='t2g: ',
+                                               source_name='text',
+                                               target_name='graph')
     model = SimpleT5()
     #model.from_pretrained("t5","t5-small")
     model.load_model('t5', model_path, use_gpu=True)
@@ -61,6 +67,7 @@ def predict_samples(model_path, test_data_path, num_samples=10):
     ref = defaultdict(list)
     ptr = 0
     same = defaultdict(list)
+    all_data_len = len(test_df)-1
     for i in range(num_samples):
         source = test_df['source_text'][i]
         prediction = model.predict(source)
@@ -74,23 +81,42 @@ def predict_samples(model_path, test_data_path, num_samples=10):
             ptr += 1
         same[ptr].append(ground_truth.lower())
         ref[i] = same[ptr]
-
         hyp[i] = [prediction[0]]
 
-    print(f"--------- Original Paper Evaluations Metrics --------------")
+    #print(f"--------- Original Paper Evaluations Metrics --------------")
     ret = bleu.compute_score(ref, hyp)
     print('BLEU INP {0:}'.format(len(hyp)))
     print('BLEU 1-4 {0:}'.format(ret[0]))
     
-    # Cannot tell what format they are expecting
-    #print('METEOR {0:}'.format(meteor.compute_score(ref, hyp)[0]))
-    #print('ROUGE_L {0:}'.format(rouge.compute_score(ref, hyp)[0]))
-    #print('Cider {0:}'.format(cider.compute_score(ref, hyp)[0]))
+    print('METEOR {0:}'.format(meteor.compute_score(ref, hyp)[0]))
+    print('ROUGE_L {0:}'.format(rouge.compute_score(ref, hyp)[0]))
+    print('Cider {0:}'.format(cider.compute_score(ref, hyp)[0]))
 
     
 
 
 if __name__ == '__main__':
+    
+    print('-----Single-Task-t2g------')
     predict_samples(
-        r'outputs\simplet5-epoch-4-train-loss-0.4691-val-loss-0.8461', 
-        'json_datasets/out_test.json')
+        r'outputs_t2g\simplet5-epoch-7-train-loss-0.0921-val-loss-0.3717',
+        'json_datasets/out_test.json',
+        mode='t2g')
+    
+    print('-----Multi-Task-t2g------')
+    predict_samples(
+        r'outputs_duo\simplet5-epoch-8-train-loss-0.3623-val-loss-0.8387',
+        'json_datasets/out_test.json',
+        mode='t2g')
+    
+    print('-----Single-Task-g2t------')
+    predict_samples(
+        r'outputs_g2t\simplet5-epoch-9-train-loss-0.795-val-loss-1.3175',
+        'json_datasets/out_test.json',
+        mode='g2t')
+
+    print('-----Multi-Task-g2t------')
+    predict_samples(
+        r'outputs_duo\simplet5-epoch-8-train-loss-0.3623-val-loss-0.8387',
+        'json_datasets/out_test.json',
+        mode='g2t')
